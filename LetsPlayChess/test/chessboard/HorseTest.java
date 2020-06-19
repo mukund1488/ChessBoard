eb7b2340f473b4968bc414464c7498e82557771b
mukund1488 <mukund4motwani@gmail.com>
2020-06-19 22:11:11 +0530

    The final piece!!! Horse is brought down!!!

---
+++ LetsPlayChess/test/chessboard/HorseTest.java
@@ -0,0 +1,25 @@
+package chessboard;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import exception.ChessBoardException;
+
+public class HorseTest {
+
+	private Horse horse = new Horse();
+
+	@Test
+	public void testPossibleMovesForHorseForProperInput() throws ChessBoardException {
+		String possibleMoves = horse.possibleMovesAvailable("E3");
+		assertEquals("D1,F1,F5,D5,C2,C4,G2,G4", possibleMoves);
+	}
+
+	@Test
+	public void testPossibleMovesForHorseForBorderScenarioOfRow() throws ChessBoardException {
+		String possibleMoves = horse.possibleMovesAvailable("A1");
+		assertEquals("B3,C2", possibleMoves);
+	}
+
+}
